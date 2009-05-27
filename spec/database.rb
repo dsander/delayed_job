fbdb9b34736f61d09d697f33f0e08ffdcda7713d
Dominik Sander <dominik@achmed>
2009-05-27 21:05:10 +0200

    Fixed some conflicts

--- spec/database.rb
+++ spec/database.rb
@@ -3,6 +3,7 @@ $:.unshift(File.dirname(__FILE__) + '/../../rspec/lib')
 
 require 'rubygems'
 require 'active_record'
+gem 'sqlite3-ruby'
 
 require File.dirname(__FILE__) + '/../init'
 require 'spec'
@@ -37,5 +38,8 @@ end
 
 # Purely useful for test cases...
 class Story < ActiveRecord::Base
-  def tell; text; end
+  def tell; text; end       
+  def whatever(n, _); tell*n; end
+  
+  handle_asynchronously :whatever
 end
